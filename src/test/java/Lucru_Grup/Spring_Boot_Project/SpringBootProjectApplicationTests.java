6657c399d2c6f114c6bcf52968603949e5f2362f
Ion Botnariuc <ionelbot23@gmail.com>
2025-02-25 19:43:16 +0200

    First modified.

---
+++ src/test/java/Lucru_Grup/Spring_Boot_Project/SpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package Lucru_Grup.Spring_Boot_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
